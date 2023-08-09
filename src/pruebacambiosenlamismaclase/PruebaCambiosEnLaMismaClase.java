d11792dd2822bd3921ccc7a98f41a1c909e613a7
Emilianoperrin88 <141650426+Emilianoperrin88@users.noreply.github.com>
2023-08-09 10:57:35 -0300

    Update PruebaCambiosEnLaMismaClase.java

--- src/pruebacambiosenlamismaclase/PruebaCambiosEnLaMismaClase.java
+++ src/pruebacambiosenlamismaclase/PruebaCambiosEnLaMismaClase.java
@@ -16,6 +16,7 @@ public class PruebaCambiosEnLaMismaClase {
      */
     public static void main(String[] args) {
         System.out.println("Hola");
+        System.out.println("Hello");
     }
     
 }
